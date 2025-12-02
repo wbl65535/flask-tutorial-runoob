66799ef1bc1b2da7a94150705f11de085633754b
wbl65535 <wbl65535@gmail.com>
2025-12-02 15:34:30 +0800

    添加 about

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route('/about')
+def about():
+    return 'This is the About Page.'
+
 if __name__ == "__main__":
-    app.run()(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
